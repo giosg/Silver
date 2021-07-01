6452bd19065fef56f2746a9a84892be9f663f357
Valdemar Lubenets <valdemar.lubenets@giosg.com>
2021-07-01 14:31:55 +0300

    Rewritten vulners module to the class with cache db initialization

--- modules/vulners.py
+++ modules/vulners.py
@@ -3,58 +3,62 @@ import json
 from core.requester import requester
 from core.utils import reader, write_json
 
-file = './db/vulners_cache.json'
-database = json.loads(reader(file))
 
-def vulners(software, version, cpe=False):
-    if software and version:
-        pass
-    else:
-        return False
-    cached = query_cache(software, version, cpe)
-    if cached:
-        if cached == 'vulnerable':
-            return True
+class VulnersExplorer:
+    def __init__(self, cache_file_path):
+        self.cache_file_path = cache_file_path
+        self.database = json.loads(reader(self.cache_file_path))
+
+
+    def vulners(self, software, version, cpe=False):
+        if software and version:
+            pass
         else:
             return False
-    kind = 'software'
-    if cpe:
-        kind = 'cpe'
-    data = '{"software": "%s", "version": "%s", "type" : "%s", "maxVulnerabilities" : %i}' % (software, version, kind, 1)
-    response = requester('https://vulners.com/api/v3/burp/software/', get=False, data=data).text
-    cache(software, version, response, cpe)
-    if 'Nothing found for Burpsuite search request' in response:
-        return False
-    return True
-
-def query_cache(software, version, cpe):
-    if cpe:
-        if software in database['by_cpe']:
-            if database['by_cpe'][software] == True:
-                return 'vulnerable'
+        cached = self.__query_cache(software, version, cpe)
+        if cached:
+            if cached == 'vulnerable':
+                return True
             else:
-                return 'not-vulerable'
+                return False
+        kind = 'software'
+        if cpe:
+            kind = 'cpe'
+        data = '{"software": "%s", "version": "%s", "type" : "%s", "maxVulnerabilities" : %i}' % (software, version, kind, 1)
+        response = requester('https://vulners.com/api/v3/burp/software/', get=False, data=data).text
+        self.__cache(software, version, response, cpe)
+        if 'Nothing found for Burpsuite search request' in response:
             return False
-    else:
-        if software in database['by_version']:
-            if version in database['by_version'][software]:
-                if database['by_version'][software][version] == True:
+        return True
+
+    def __query_cache(self, software, version, cpe):
+        if cpe:
+            if software in self.database['by_cpe']:
+                if self.database['by_cpe'][software] == True:
                     return 'vulnerable'
                 else:
                     return 'not-vulerable'
-            return False
-    return False
+                return False
+        else:
+            if software in self.database['by_version']:
+                if version in self.database['by_version'][software]:
+                    if self.database['by_version'][software][version] == True:
+                        return 'vulnerable'
+                    else:
+                        return 'not-vulerable'
+                return False
+        return False
 
-def cache(software, version, response, cpe):
-    vulnerable = True
-    if 'Nothing found for Burpsuite search request' in response:
-        vulnerable = False
-    if cpe:
-        if software not in database['by_cpe']:
-            database['by_cpe'][software] = vulnerable
-    else:
-        if software not in database['by_version']:
-            database['by_version'][software] = {}
-        if version not in database['by_version'][software]:
-            database['by_version'][software][version] = vulnerable
-    write_json(file, database)
+    def __cache(self, software, version, response, cpe):
+        vulnerable = True
+        if 'Nothing found for Burpsuite search request' in response:
+            vulnerable = False
+        if cpe:
+            if software not in self.database['by_cpe']:
+                self.database['by_cpe'][software] = vulnerable
+        else:
+            if software not in cache_db['by_version']:
+                self.database['by_version'][software] = {}
+            if version not in database['by_version'][software]:
+                self.database['by_version'][software][version] = vulnerable
+        write_json(self.cache_file_path, self.database)
