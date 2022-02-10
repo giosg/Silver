e0cd4e4d8956fa9c98c579a520d03cbca737c338
Valdemar Lubenets <valdemar.lubenets@giosg.com>
2022-02-10 15:34:06 +0200

    Add slack pagination with one message not more than 50 blocks

--- notifications/notifiers.py
+++ notifications/notifiers.py
@@ -4,8 +4,15 @@ from notifications.hostinfo import HostInfo
 from typing import List, NoReturn
 from settings import SETTINGS
 
+MAX_SLACK_BLOCK_COUNT_ALLOWED = 50
+
 def send_report_to_slack(slack_client: WebhookClient, hosts: List[HostInfo]) -> NoReturn:
-  slack_client.send(blocks=get_notification_template_slack_block(hosts))
+  blocks = get_notification_template_slack_block(hosts)
+  c_begin, c_end = 0, MAX_SLACK_BLOCK_COUNT_ALLOWED
+  while c_begin < len(blocks):
+    slack_client.send(blocks=blocks[c_begin:c_end])
+    c_begin += MAX_SLACK_BLOCK_COUNT_ALLOWED
+    c_end += MAX_SLACK_BLOCK_COUNT_ALLOWED
 
 def notify(hosts: List[HostInfo]) -> NoReturn:
   if SETTINGS.SLACK_WEBHOOK:
